61a72505003ffca3154f4505a04fbbd7c49b81f0
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-09-05 01:27:29 -0700

    Change html output title to Sudoku Checker

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,42 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // new line
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Sudoku Checker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
     },
-    devtool: 'eval-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new UglifyJsPlugin({
-            sourceMap: true
-        }),
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Ping Pong',
-            template: './src/index.html',
-            inject: 'body'
-        })
-    ],
-    module: {
-        rules: [{
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: [
-                    /node_modules/,
-                    /spec/
-                ],
-                loader: "eslint-loader"
-            }
-        ]
+    {
+      test: /\.js$/,
+      exclude: [
+        /node_modules/,
+        /spec/
+      ],
+      loader: "eslint-loader"
     }
+    ]
+  }
 };
